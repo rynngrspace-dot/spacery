f241c2b3afc711e73120604327576ec4cbfc7564
rynngrspace-dot <rynngrspace@gmail.com>
2026-04-25 05:44:05 -0700

    fix: pwa

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,9 @@ const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
-  skipWaiting: true,
+  workboxOptions: {
+    skipWaiting: true,
+  },
 });
 
 const nextConfig: NextConfig = {
